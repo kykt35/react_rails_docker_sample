19e37ca309e5adfc74b6d4aabf010bd8cdc0ab5e
kykt35 <kykt35@gmail.com>
2021-01-17 12:30:57 +0900

    Setup typescript

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import { AppProps } from "next/app";
+import '../styles/globals.css'
+
+
+function MyApp(props: AppProps) {
+    const { Component, pageProps } = props;
+
+  return <Component {...pageProps} />
+}
+
+export default MyApp
